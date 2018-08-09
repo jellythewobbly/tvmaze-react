1c19bd3b453ec7782fe2ec54a962ba3919cd942f
jellythewobbly <jellythewobbly@hotmail.com>
2018-08-09 23:01:40 +0800

    Done basic show app

--- index.jsx
+++ index.jsx
@@ -1,66 +1,105 @@
-class App extends React.Component {
-    render() {
-        return (
-            <div className="container text-center">
-                <h1 className="my-2">TV MAZE</h1>
-                <Input />
-            </div>
-        )
-    }
+class Home extends React.Component {
+	render() {
+		return (
+			<div className="container text-center">
+				<h1 className="my-2">TVMaze React</h1>
+				<Search />
+			</div>
+		);
+	}
 }
 
-class Input extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            placeholder: 'Search Here',
-            inputValue: '',
-            showResults: false
-        };
-    }
+class Search extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			placeholder: 'Enter a Movie Title...',
+			inputValue: '',
+			showResults: false
+		};
+	}
 
-    handleInputChange = event => {
-        this.setState({inputValue : event.target.value});
-    }
+	handleInputChange = event => {
+		this.setState({ inputValue: event.target.value });
+	};
 
-    handleSubmit = () => {
-        this.setState({
-            inputValue: '',
-            showResults: true
-        });
-    }
+	handleSubmit = () => {
+		this.setState({
+			inputValue: '',
+			showResults: true
+		});
+	};
 
-    render() {
-        return (
-            <div>
-                <input className="form-control w-50 mx-auto my-2" name="textBox" placeholder={this.state.placeholder} value={this.state.inputValue} onChange={this.handleInputChange} />
-                <input className="btn btn-primary w-25 mx-auto my-2" type="submit" onClick={this.handleSubmit} value="Search" />
-                {this.state.showResults &&
-                <QueryResults />
-                }
-            </div>
-        );
-    }
+	searchAgain = () => {
+		this.setState({
+			showResults: false
+		});
+	};
+
+	render() {
+		return (
+			<div>
+				{!this.state.showResults && (
+					<React.Fragment>
+						<input
+							className="form-control w-50 mx-auto my-2"
+							name="textBox"
+							placeholder={this.state.placeholder}
+							value={this.state.inputValue}
+							onChange={this.handleInputChange}
+						/>
+						<input
+							className="btn btn-primary w-25 mx-auto my-2"
+							type="submit"
+							onClick={this.handleSubmit}
+							value="Search"
+						/>
+					</React.Fragment>
+				)}
+
+				{this.state.showResults && (
+					<React.Fragment>
+						<input
+							className="btn btn-primary w-25 mx-auto my-2"
+							type="submit"
+							onClick={this.searchAgain}
+							value="Search Again"
+						/>
+						<Results />
+					</React.Fragment>
+				)}
+			</div>
+		);
+	}
 }
 
-class QueryResults extends React.Component {
-    render() {
-        let shows = results.map(show => {
-            return (
-                <div className="col-lg-2 mx-2" key={show.show.id}>
-                    <div className="card" style={{width: '12rem'}}>
-                    <img className="card-img-top" src={show.show.image.medium} />
-                    <h4 className="card-title">{show.show.name}</h4>
-                    </div>
-                </div>
-            );
-        });
-    
-        return (
-            <div className="row my-5">{shows}</div>
-        );
-    }
+class Results extends React.Component {
+	render() {
+		let shows = results.map(show => {
+			return (
+				<Result
+					key={show.show.id}
+					img={show.show.image.medium}
+					name={show.show.name}
+				/>
+			);
+		});
+
+		return <div className="row my-5">{shows}</div>;
+	}
 }
 
+class Result extends React.Component {
+	render() {
+		return (
+			<div className="col-lg-2 mx-2">
+				<div className="card" style={{ width: '12rem' }}>
+					<img className="card-img-top" src={this.props.img} />
+					<h4 className="card-title">{this.props.name}</h4>
+				</div>
+			</div>
+		);
+	}
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Home />, document.getElementById('root'));
